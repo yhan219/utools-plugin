6a0f8b82f18ba0fdfb3b0005adee809ee2786cf0
yhan219 <yhan219@163.com>
2020-08-11 21:29:40 +0800

    有道翻译

---
+++ youdao-translate/README.md
@@ -0,0 +1,23 @@
+## 有道翻译
+
+对于大部分人，搜索结果往往需要直接复制到粘贴板，特此开发了此插件
+
+插件开源地址 [utools-plugin](https://github.com/yhan219/utools-plugin)
+
+### 功能介绍
+- 进入显示搜索历史(todo)
+
+- 输入关键字搜索（自动识别中英文）
+![pj8RPP](https://gitee.com/yhan219/blog-image/raw/master/yhan/pj8RPP.jpg)
+
+- 选中回车即可复制翻译结果
+
+### 设置
+由于有道翻译需要收费，所以需要用户手动设置appId和秘钥，每个新注册的用户都有50元体验金，对于个人来说已经够用了，即使用完了也可以重新注册个账号。
+- 打开网址[有道开放平台](http://ai.youdao.com/login.s)
+- 登录成功后，点击`应用管理--我的应用`，没有应用的创建一个即可
+- 点击进入应用后即可看到应用id和应用秘钥
+   -    如图：
+  ![应用](https://gitee.com/yhan219/blog-image/raw/master/yhan/aLHCYP.png)
+- 在utools主窗口输入 `有道appid`,回车进入，再将复制的appId输入即可
+- 在utools主窗口输入 `有道秘钥`,回车进入，再将复制的秘钥输入即可
---
+++ youdao-translate/preload.js
@@ -0,0 +1,248 @@
+const {clipboard} = require('electron')
+const https = require('https');
+const querystring = require('querystring');
+const SHA256 = require("./sha256");
+const errorCode = require("./error_code").errorCode
+
+const noData = [{
+    title: '没有数据',
+    icon: './logo.png'
+}]
+
+const noAppId = [{
+    title: '未设置app id，请阅读详情并按步骤设置',
+    icon: './logo.png'
+}]
+
+
+const noAppSecret = [{
+    title: '未设置app secret，请阅读详情并按步骤设置≤',
+    icon: './logo.png'
+}]
+
+function truncate(q) {
+    var len = q.length;
+    if (len <= 20) return q;
+    return q.substring(0, 10) + len + q.substring(len - 10, len);
+}
+
+function getFrom(searchWord) {
+    const reg = /^[A-Za-z]+.*$/;
+    if (reg.test(searchWord)) {
+        return 'en'
+    }
+    return 'zh-CHS';
+}
+
+function getTo(from) {
+    if (from === 'en') {
+        return 'zh-CHS';
+    }
+    return 'en';
+}
+
+
+function handleSearch(searchWord, callbackSetList) {
+    const appId = getAppId(callbackSetList);
+    if (appId === '') {
+        return
+    }
+    const appSecret = getAppSecret(callbackSetList);
+    if (appSecret === '') {
+        return
+    }
+    const salt = (new Date()).getTime();
+    const curtime = Math.round(new Date().getTime() / 1000);
+    const from = getFrom(searchWord);
+    const to = getTo(from);
+    // var to = 'en';
+    const str1 = appId + truncate(searchWord) + salt + curtime + appSecret;
+
+    const sign = SHA256(str1);
+    const param = {
+        q: searchWord,
+        appKey: appId,
+        salt: salt,
+        from: from,
+        to: to,
+        sign: sign,
+        signType: "v3",
+        curtime: curtime
+    };
+
+    https.get({
+            hostname: 'openapi.youdao.com',
+            path: '/api?' + querystring.stringify(param),
+            timeout: 30000
+        }, (res) => {
+            res.on('data', function (data) {
+                let jsonData = JSON.parse(data);
+                if (jsonData.errorCode !== '0') {
+                    callbackSetList([{
+                        title: errorCode.get(jsonData.errorCode),
+                        description: '',
+                        icon: './logo.png'
+                    }])
+                    return
+                }
+                let items = [];
+                if (jsonData.translation) {
+                    jsonData.translation.forEach(item => {
+                        items.push({
+                            title: item,
+                            description: searchWord,
+                            icon: './logo.png'
+                        });
+                    })
+                }
+                let exist = items.map(item => item.title);
+                if (jsonData.web) {
+                    jsonData.web.forEach(item => {
+                        let desc = item.key;
+                        item.value.forEach(v => {
+                            if (exist.includes(v)) {
+                                return
+                            }
+                            items.push({
+                                title: v,
+                                description: desc,
+                                icon: './logo.png'
+                            });
+                        })
+                    });
+                }
+                if (items.length === 0) {
+                    callbackSetList(noData)
+                    return
+                }
+                callbackSetList(items);
+            });
+        }
+    )
+    ;
+}
+
+
+String.prototype.resetBlank = function () {
+    var regEx = /\s+/g;
+    return this.replace(regEx, ' ');
+};
+
+const handleResult = function (itemData) {
+    if (itemData.title) {
+        clipboard.writeText(itemData.title, 'selection');
+    }
+};
+
+
+const saveOrUpdateDb = function (key, data) {
+    if (!data) {
+        return
+    }
+    let keyData = window.utools.db.get(key);
+    if (!keyData) {
+        window.utools.db.put({
+            _id: key,
+            data: data
+        })
+        return
+    }
+    utools.db.put({
+        _id: key,
+        data: data,
+        _rev: keyData._rev
+    })
+};
+
+const getAppId = function (callbackSetList) {
+    let appId = window.utools.db.get("app_id");
+    if (!appId) {
+        callbackSetList(noAppId)
+        return ''
+    }
+    return appId.data
+};
+
+const getAppSecret = function (callbackSetList) {
+    let appSecret = window.utools.db.get("app_secret");
+    if (!appSecret) {
+        callbackSetList(noAppSecret)
+        return ''
+    }
+    return appSecret.data
+};
+
+window.exports = {
+    "youdao-translate": {
+        mode: "list",
+        args: {
+            // 进入插件时调用（可选）
+            enter: (action, callbackSetList) => {
+                // todo 搜索历史
+            },
+            // 子输入框内容变化时被调用 可选 (未设置则无搜索)
+            search: (action, searchWord, callbackSetList) => {
+                if (searchWord) {
+                    handleSearch(searchWord, callbackSetList);
+                }
+            },
+            // 用户选择列表中某个条目时被调用
+            select: (action, itemData, callbackSetList) => {
+                window.utools.hideMainWindow()
+                handleResult(itemData)
+                window.utools.outPlugin()
+            },
+            placeholder: "搜索回车复制"
+        }
+    },
+    "youdao-app-id": {
+        mode: "list",
+        args: {
+            enter: (action, callbackSetList) => {
+                //显示当前app id
+                let id = getAppId(callbackSetList);
+                if (id !== '') {
+                    callbackSetList({
+                        title: id
+                    });
+                }
+            },
+            // 子输入框内容变化时被调用 可选 (未设置则无搜索)
+            search: (action, searchWord, callbackSetList) => {
+                saveOrUpdateDb('app_id', searchWord);
+            },
+            // 用户选择列表中某个条目时被调用
+            select: (action, itemData, callbackSetList) => {
+                window.utools.hideMainWindow()
+                handleResult(itemData)
+                window.utools.outPlugin()
+            },
+            placeholder: "输入appId并保存"
+        }
+    },
+    "youdao-app-secret": {
+        mode: "list",
+        args: {
+            enter: (action, callbackSetList) => {
+                //显示当前app id
+                let secret = getAppSecret(callbackSetList);
+                if (secret !== '') {
+                    callbackSetList({
+                        title: secret
+                    });
+                }
+            },
+            // 子输入框内容变化时被调用 可选 (未设置则无搜索)
+            search: (action, searchWord, callbackSetList) => {
+                saveOrUpdateDb("app_secret", searchWord);
+            },
+            // 用户选择列表中某个条目时被调用
+            select: (action, itemData, callbackSetList) => {
+                window.utools.hideMainWindow()
+                handleResult(itemData)
+                window.utools.outPlugin()
+            },
+            placeholder: "输入secret并保存"
+        }
+    }
+}
